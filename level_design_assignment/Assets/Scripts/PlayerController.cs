7f3dd6e75a59f283e3b01b69ba1bea5ce7cea193
FishFood6 <l.g.van.den.brink@gmail.com>
2021-09-16 10:03:00 +0200

    added playercontroller

---
+++ level_design_assignment/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed;
+    [SerializeField]
+    private float JumpHeight;
+
+    private int count = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void OnJump()
+    {
+        Vector3 Jump = new Vector3(0.0f, JumpHeight, 0.0f);
+        rb.AddForce(Jump, ForceMode.Impulse);
+        Debug.Log("Jump");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * Speed);
+    }
+}
