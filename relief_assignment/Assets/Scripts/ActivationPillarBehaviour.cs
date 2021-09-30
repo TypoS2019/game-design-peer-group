648fd198fe4592dcc8734fddb4a77db02e95357a
Steppe Schoolkate <S.Schoolkate@student.han.nl>
2021-09-30 09:47:39 +0200

    added interactable

---
+++ relief_assignment/Assets/Scripts/ActivationPillarBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationPillarBehaviour : MonoBehaviour
+{
+    [SerializeField] private ActivationPillarBehaviour previousPillar;
+    [SerializeField] private GameObject flame;
+    
+    public delegate void ActivatedIncorrectly();
+    public static event ActivatedIncorrectly OnActivatedIncorrectly;
+    
+    public bool active;
+
+    void Start()
+    {
+        OnActivatedIncorrectly += Deactivate;
+        Deactivate();
+    }
+
+    public void Activate()
+    {
+        if (previousPillar.active)
+        {
+            flame.SetActive(true);
+            active = true;
+        }
+        else
+        {
+            OnActivatedIncorrectly!.Invoke();
+            flame.SetActive(false);
+            active = false;
+        }
+    }
+
+    public void Deactivate()
+    {        
+        flame.SetActive(false);
+        active = false;
+    }
+}
